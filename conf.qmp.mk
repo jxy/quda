5fd1b801322ac9ab96ba38b92869cdadd7353697
Xiao-Yong Jin <xjin@anl.gov>
2021-09-24 14:49:03 +0000

    omp-extra: add QIO to conf.qmp.mk

--- conf.qmp.mk
+++ conf.qmp.mk
@@ -1,12 +1,16 @@
 CXX=mpicxx
 
-CXXFLAGS=$CXXFLAGS -DQMP_COMMS -DMULTI_GPU
+CXXFLAGS=$CXXFLAGS -DQMP_COMMS -DMULTI_GPU -DHAVE_QIO
 CXXFLAGS=$CXXFLAGS -I../qio/include -I../qmp/include
 
 LDFLAGS=$LDFLAGS\
 	-L../qio/lib\
 	-lqio -llime\
 	-L../qmp/lib\
-	-lqmp
+	-lqmp\
 
-LIBOFILES=lib/communicator_qmp.o
+LIBOFILES=$LIBOFILES\
+	lib/communicator_qmp.o\
+	lib/layout_hyper.o\
+	lib/qio_field.o\
+
